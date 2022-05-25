0975f530c0a0102c18ee0624222ca9bc1ea0d02a
Tommy Vo <votommy@outlook.com>
2022-05-24 20:55:41 -0500

    Create Plus_One.js

---
+++ javascript/Plus_One.js
@@ -0,0 +1,16 @@
+/*
+You are given a large integer represented as an integer array 'digits'. The large integer does not contain any leading 0's.
+Increment the large integer by one and return the resulting array of digits.
+*/
+
+var plusOne = function(digits) {
+    let wholeNum = "";
+    for(let i = 0; i < digits.length; i++)
+        wholeNum += digits[i];
+    
+    let wholeNumInt = BigInt(wholeNum);
+    wholeNumInt++;
+    wholeNumInt += "";
+    
+    return wholeNumInt.split("");
+};
